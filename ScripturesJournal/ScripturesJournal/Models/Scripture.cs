a06b4f796cad74a6d5aa43ea2c718dc9ad034578
Elías Monzón Castro <elidarson@hotmail.com>
2018-11-10 17:13:45 -0300

    Tutorial finished. Page working.

--- ScripturesJournal/ScripturesJournal/Models/Scripture.cs
+++ ScripturesJournal/ScripturesJournal/Models/Scripture.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ScripturesJournal.Models
 {
     public class Scripture
     {
         public int ID { get; set; }
+
+        [Display(Name = "Registration Date")]
+        [DataType(DataType.Date)]
         public DateTime RegistrationDate { get; set; }
+
+        [StringLength(40, MinimumLength = 3)]
+        [Display(Name = "Book")]
+        [Required]
         public string Book { get; set; }
+
+        [Range(1, 200)]
+        [Display(Name = "Chapter")]
         public int Chapter { get; set; }
+
+        [Range(1, 200)]
+        [Display(Name = "Verse")]
         public int Verse { get; set; }
+
+        [StringLength(1000, MinimumLength = 3)]
+        [Display(Name = "Notes")]
         public string Notes { get; set; }
     }
 }
--- ScripturesJournal/ScripturesJournal/Pages/Scriptures/Index.cshtml.cs
+++ ScripturesJournal/ScripturesJournal/Pages/Scriptures/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ScripturesJournal.Models;
 
@@ -19,10 +20,33 @@ namespace ScripturesJournal.Pages.Scriptures
         }
 
         public IList<Scripture> Scripture { get;set; }
+        public string SearchString { get; set; }
+        public SelectList Book { get; set; }
+        public string ScriptureBook { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string scriptureBook, string searchString)
         {
-            Scripture = await _context.Scripture.ToListAsync();
+            // Use LINQ to get list of books.
+            IQueryable<string> bookQuery = from m in _context.Scripture
+                                            orderby m.Book
+                                            select m.Book;
+            
+            // using System.Linq to select the scriptures;
+            var scriptures = from m in _context.Scripture
+                             select m;
+
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                scriptures = scriptures.Where(s => s.Book.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(scriptureBook))
+            {
+                scriptures = scriptures.Where(x => x.Book == scriptureBook);
+            }
+            Book = new SelectList(await bookQuery.Distinct().ToListAsync());
+            Scripture = await scriptures.ToListAsync();
+            SearchString = searchString;
         }
     }
 }
